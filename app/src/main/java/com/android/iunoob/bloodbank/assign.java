0f033bb88cee25ed14b0bc2d86216826447b5d03
proma41 <farhanatazrin25@gmail.com>
2023-03-31 07:28:37 +0800

    new added

---
+++ app/src/main/java/com/android/iunoob/bloodbank/assign.java
@@ -0,0 +1,17 @@
+package com.android.iunoob.bloodbank;
+
+public class assign {
+    String CSE;
+
+    public assign(String CSE) {
+        this.CSE = CSE;
+    }
+
+    public String getCSE() {
+        return CSE;
+    }
+
+    public void setCSE(String CSE) {
+        this.CSE = CSE;
+    }
+}
